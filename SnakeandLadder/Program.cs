b63e645d7ddd54185cce90261cf4afd1196b6c5a
vishnu244 <vishnunali244@gmail.com>
2022-04-21 19:27:15 +0530

    Repeating till 100

--- SnakeandLadder/Program.cs
+++ SnakeandLadder/Program.cs
@@ -3,28 +3,37 @@ Console.WriteLine("Welcome to Snake and Ladder!");
 
 int playerposition = 0;
 Random random = new Random();
-int rollDie = random.Next(1, 7);
-Console.WriteLine(rollDie);
-int options = random.Next(0, 3);
-switch (options)
+int isWin = 100;
+while (playerposition <= isWin)
 {
-    case 0:
-        Console.WriteLine("NO_PLAY");
-        playerposition = 0;
-        break;
-    case 1:
-        Console.WriteLine("LADDER");
-        playerposition += rollDie;
-        break;
-    case 2:
-        Console.WriteLine("SNAKE");
-        playerposition -= rollDie;
-        if (playerposition < 0)
-        {
+    int rollDie = random.Next(1, 7);
+    int options = random.Next(0, 3);
+    switch (options)
+    {
+        case 0:
+            Console.WriteLine("NO_PLAY");
             playerposition = 0;
-        }
-        break;
-    default:
-        break;
+            break;
+        case 1:
+            Console.WriteLine("LADDER");
+            playerposition += rollDie;
+            break;
+        case 2:
+            Console.WriteLine("SNAKE");
+            playerposition -= rollDie;
+            if (playerposition < 0)
+            {
+                playerposition = 0;
+            }
+            break;
+        default:
+            break;
+    }
+
+    Console.WriteLine(playerposition);
 }
 Console.WriteLine(playerposition);
+if (playerposition == isWin)
+{
+    Console.WriteLine("You Won!!");
+}
