ee8be6a65fb0803a7b652fd07e94b091acd1fbb1
vishnu244 <vishnunali244@gmail.com>
2022-04-21 19:24:55 +0530

    checking the condition

--- SnakeandLadder/Program.cs
+++ SnakeandLadder/Program.cs
@@ -1,8 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to Snake and Ladder!");
 
-
 int playerposition = 0;
 Random random = new Random();
 int rollDie = random.Next(1, 7);
 Console.WriteLine(rollDie);
+int options = random.Next(0, 3);
+switch (options)
+{
+    case 0:
+        Console.WriteLine("NO_PLAY");
+        playerposition = 0;
+        break;
+    case 1:
+        Console.WriteLine("LADDER");
+        playerposition += rollDie;
+        break;
+    case 2:
+        Console.WriteLine("SNAKE");
+        playerposition -= rollDie;
+        if (playerposition < 0)
+        {
+            playerposition = 0;
+        }
+        break;
+    default:
+        break;
+}
+Console.WriteLine(playerposition);
