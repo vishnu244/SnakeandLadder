c17330de12f4cdcef16cd1cca008dcc8fe51040b
vishnu244 <vishnunali244@gmail.com>
2022-04-21 19:22:40 +0530

    Roll the die

--- SnakeandLadder/Program.cs
+++ SnakeandLadder/Program.cs
@@ -1,5 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to Snake and Ladder!");
 
-int playerPosition = 0;
-Console.WriteLine(playerPosition);
+
+int playerposition = 0;
+Random random = new Random();
+int rollDie = random.Next(1, 7);
+Console.WriteLine(rollDie);
